ac09b95445dae2d58cf44a21f5d571978853614d
Alessandro Di Crescenzo <adicrescenzo79@gmail.com>
2021-03-25 18:24:19 +0100

    completo

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,23 +4,59 @@ var app = new Vue({
     input: '',
     todos: [
       {
-        title: 'ciao',
-        status: 'todo',
+        title: 'già fatto',
+        status: 'done',
+        editing: false,
       },
+      {
+        title: 'ancora da fare',
+        status: 'todo',
+        editing: false,
+      },
+      {
+        title: 'ancora da fare tre volte',
+        status: 'todo',
+        editing: false,
+      },
+
+
     ],
   },
+  computed: {
+    todosOrdered: function (){
+      let todosTodo = this.todos.filter((todo) => todo.status == 'todo');
+      let todosDone = this.todos.filter((todo) => todo.status == 'done');
 
-  methods: {
-    submit: function(){
-      let newTodo = {
-        title: this.input,
-        status: 'todo',
-      }
-      this.todos.push(newTodo);
-      this.input = ''
+      return [...todosTodo, ...todosDone]
     }
   },
-  mounted () {
-  }
+  methods: {
+    submit: function(){
+
+      if (this.input != '') {
+        let newTodo = {
+          title: this.input,
+          status: 'todo',
+        }
+        this.todos.push(newTodo);
+        this.input = '';
+      }
+    },
+
+    fatto: function(todo){
+      let index = this.todos.indexOf(todo);
+      this.todos[index].status = 'done';
+    },
+    modifica: function(todo){
+      let index = this.todos.indexOf(todo);
+      this.input = todo.title;
+      this.todos.splice(index, 1);
+
+    },
+    remove: function(todo){
+      let index = this.todos.indexOf(todo);
+      this.todos.splice(index, 1)
+    }
+  },
 
 })
